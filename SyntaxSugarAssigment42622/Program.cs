2bf526841ad955383803b192a4e03f095335f49f
Gustavo Gonzalez <gusgonzalez.gg@gmail.com>
2022-04-26 11:47:06 -0400

    commit for syntax exercise

---
+++ SyntaxSugarAssigment42622/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SyntaxSugarAssigment42622
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //The commented out code is the assignment to be changed
+
+            //int answer = 4;
+            // string response;
+            //if (answer < 9)
+            // {
+            //     response = answer + " is less than nine";
+            // }
+            // else
+            // {
+            //     response = answer + " is greater than or equal to nine";
+            // }
+
+
+            var answer = 4;
+            var response = (answer > 9) ? "This is more than 9" : "This is less than 9";
+
+            Console.WriteLine(response);
+        }
+    }
+}
